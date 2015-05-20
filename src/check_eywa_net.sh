5ed32a5014291252664a8b30f25f1f118063f6be
JungJungIn <call518@gmail.com>
2015-05-20 19:51:00 +0900

    update src/check_eywa_net.sh, install.sh

--- src/check_eywa_net.sh
+++ src/check_eywa_net.sh
@@ -7,7 +7,6 @@ DB_PASS="@@__ONEADMIN_PW__@@"
 MYSQL_EYWA="mysql -u$DB_USER -p$DB_PASS -h$DB_HOST $DB_NAME -s -N"
 
 EYWA_VID=`grep "one-[0-9]" $domain | sed -e 's/<name>//g' | sed -e 's/<\/name>//g' | cut -d- -f2`
-echo $EYWA_VID > /tmp/111
 
 EYWA_UID=`$MYSQL_EYWA -e "select uid from vm_info where vid='$EYWA_VID'"`
 EYWA_NUM=`$MYSQL_EYWA -e "select num from mc_address where uid='$EYWA_UID'"`
