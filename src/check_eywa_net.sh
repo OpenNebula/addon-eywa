8a58884572a34f6912ed0019399247d53d0b84a9
JungJungIn <call518@gmail.com>
2015-05-21 16:51:24 +0900

    update check_eywa_net.sh: add if-then

--- src/check_eywa_net.sh
+++ src/check_eywa_net.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-DB_HOST="@@__FRONT_IP__@@"
-DB_NAME="eywa"
-DB_USER="eywa"
-DB_PASS="@@__ONEADMIN_PW__@@"
-MYSQL_EYWA="mysql -u$DB_USER -p$DB_PASS -h$DB_HOST $DB_NAME -s -N"
-
 EYWA_VID=`grep "one-[0-9]" $domain | sed -e 's/<name>//g' | sed -e 's/<\/name>//g' | cut -d- -f2`
-while true
-do
-	EYWA_UID=`$MYSQL_EYWA -e "select uid from vm_info where vid='$EYWA_VID'"`
-	if [ "x$EYWA_UID" != "x" ]; then
-		break
-	fi
-	sleep 1
-done
-EYWA_NUM=`$MYSQL_EYWA -e "select num from mc_address where uid='$EYWA_UID'"`
+CONTEXT_FILE="/var/lib/one/vms/${EYWA_VID}/context.sh"
+source ${CONTEXT_FILE}
 
-while ! $(ifconfig VSi${EYWA_NUM} >/dev/null 2>/dev/null)
-do
-	sleep 1
-done
+if [ ${IS_EYWA} == "yes" ]; then
+    DB_HOST="@@__FRONT_IP__@@"
+    DB_NAME="eywa"
+    DB_USER="eywa"
+    DB_PASS="@@__ONEADMIN_PW__@@"
+    MYSQL_EYWA="mysql -u$DB_USER -p$DB_PASS -h$DB_HOST $DB_NAME -s -N"
+    
+    while true
+    do
+    	EYWA_UID=`$MYSQL_EYWA -e "select uid from vm_info where vid='$EYWA_VID'"`
+    	if [ "x$EYWA_UID" != "x" ]; then
+    		break
+    	fi
+    	sleep 1
+    done
+    EYWA_NUM=`$MYSQL_EYWA -e "select num from mc_address where uid='$EYWA_UID'"`
+    
+    while ! $(ifconfig VSi${EYWA_NUM} >/dev/null 2>/dev/null)
+    do
+    	sleep 1
+    done
+fi
