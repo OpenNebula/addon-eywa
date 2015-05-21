3f1b513a264182050e6d563a0d5fd28d6959caec
JungJungIn <call518@gmail.com>
2015-05-21 19:42:34 +0900

    update check_eywa_net.sh

--- src/check_eywa_net.sh
+++ src/check_eywa_net.sh
@@ -4,7 +4,7 @@ EYWA_VID=`grep "one-[0-9]" $domain | sed -e 's/<name>//g' | sed -e 's/<\/name>//
 TMP_FILE=`mktemp`
 trap "rm -f $TMP_FILE" EXIT
 CONTEXT_FILE="/var/lib/one/vms/${EYWA_VID}/context.sh"
-su -l oneadmin -c "ssh -l oneadmin @@__FRONT_IP__@@ 'cat $CONTEXT_FILE'" > $TMP_FILE
+scp @@__FRONT_IP__@@:$CONTEXT_FILE $TMP_FILE
 
 source $TMP_FILE
 
